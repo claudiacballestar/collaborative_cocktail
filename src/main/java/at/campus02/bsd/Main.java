94067a9236687dfc41d1bc7e9c4e7fda04100415
claudiacballestar <12003750@edu.campus02.at>
2026-07-16 13:44:22 +0200

    add java project with classes, pom and git

---
+++ src/main/java/at/campus02/bsd/Main.java
@@ -0,0 +1,9 @@
+package at.campus02.bsd;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+    }
+}
